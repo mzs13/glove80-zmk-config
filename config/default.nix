84fc73cb9cec3afe5037dc93275e7d18ddb85841
Matt Sochacki <matt.sochacki@gmail.com>
2026-02-21 16:57:24 -0600

    vim changes relative to base config

--- config/default.nix
+++ config/default.nix
@@ -9,11 +9,18 @@ let
   gatedKeys =  /zmk-modules/zmk-gated-key;
   vim =  /zmk-modules/zmk-vim;
 
-  tristate = pkgs.fetchFromGitHub {
-    owner = "dhruvinsh";
-    repo = "zmk-tri-state";
-    rev = "ebbc1f0ccdb51669650bb0ac3e34920d09e62400";
-    sha256 = "sha256-f+8zBdqKGlFFdXZ/UkSgc+HZWDfGzKyw/VjMqdwHYok=";
+  adaptKey = pkgs.fetchFromGitHub {
+    owner = "urob";
+    repo = "zmk-adaptive-key";
+    rev = "v0.3.0";
+    sha256 = "sha256-9c/YQlgc9guG8XmkT0pgr+ZkDVVxyhDabyd2UaZu0RQ=";
+  };
+
+  leaderKey = pkgs.fetchFromGitHub {
+    owner = "urob";
+    repo = "zmk-leader-key";
+    rev = "legacy";
+    sha256 = "sha256-6jmzby7fyNmZJTNCAT9j5+l/bbMpWEC6j5wbhVH6yFc=";
   };
 
   autoLayer = pkgs.fetchFromGitHub {
@@ -23,10 +30,26 @@ let
     sha256 = "sha256-a7SKPhEJpSha0IETeue3YM2vVF07sqEHFtDWdvyCt/s=";
   };
 
-  myModules = [
-                 gatedKeys
-                 tristate
-                 autoLayer
+  tristate = pkgs.fetchFromGitHub {
+    owner = "dhruvinsh";
+    repo = "zmk-tri-state";
+    rev = "ebbc1f0ccdb51669650bb0ac3e34920d09e62400";
+    sha256 = "sha256-f+8zBdqKGlFFdXZ/UkSgc+HZWDfGzKyw/VjMqdwHYok=";
+  };
+
+  listeners = pkgs.fetchFromGitHub {
+    owner = "ssbb";
+    repo = "zmk-listeners";
+    rev = "7f9c5c33418589bb563150855c5de282bb3e1f5b";
+    sha256 = "sha256-oey5BXS+2sg0GnOKUFc0o/ya/ux+bR3OPvlZ05akLvY=";
+  };
+
+
+  myModules = [ autoLayer
+                adaptKey
+                tristate
+                listeners
+                gatedKeys
               ];
 
   glove80_left  = firmware.zmk.override {
